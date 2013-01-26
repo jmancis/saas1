cfd62e1a0925df6b533656b918021ce16c896408
jmancis <jmancis@gmail.com>
2013-01-26 23:10:09 +0100

    Update hw1_part5.rb

--- hw1_part5.rb
+++ hw1_part5.rb
@@ -3,6 +3,14 @@ class Class
     attr_name = attr_name.to_s   # make sure it's a string
     attr_reader attr_name        # create the attribute's getter
     attr_reader attr_name+"_history" # create bar_history getter
-    class_eval "your code here, use %Q for multiline strings"
+    class_eval %Q[
+  	  def #{attr_name}=(new_value)
+			if  @#{attr_name}_history==nil
+			 @#{attr_name}_history=[nil]
+			end
+			@#{attr_name}_history<<new_value
+			@#{attr_name} = new_value
+		  end
+		]
   end
 end
