33d07a7a26103af3c92ef47ce8f73c81786d1784
jmancis <jmancis@gmail.com>
2013-01-26 20:58:12 +0100

    Update hw1_part4.rb

---
+++ hw1_part4.rb
@@ -0,0 +1,36 @@
+
+class Dessert
+  def initialize(name, calories)
+    @name= name
+    @calories=calories
+  end
+  
+  attr_accessor :name
+  attr_accessor :calories
+  
+  def healthy?
+    return (@calories < 200)
+  end
+  
+  def delicious?
+    return true
+  end
+  
+  
+end
+
+class JellyBean < Dessert
+  def initialize(name, calories, flavor)
+    super(name, calories)
+    @flavor = flavor
+  end
+  
+  attr_accessor :flavor
+  
+  def delicious?
+    return (@flavor !="black licorice")
+  end
+  
+ 
+end
+
