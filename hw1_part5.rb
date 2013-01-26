db4cdb1f7f1c03cdd89a47d571a97210ea2bf4a4
jmancis <jmancis@gmail.com>
2013-01-26 19:41:27 +0100

    Update hw1_part5.rb

--- hw1_part5.rb
+++ hw1_part5.rb
@@ -1,29 +1,8 @@
-def combine_anagrams(words)
-  if words.length==0 
-		return []
-	end
-  anagrams=[]
-   words.each do |word|
-	found = false
-      anagrams.each do |group|
-	if areAnagram(group[0],word) 
-		found=true
-		if !group.include?(word)
-			group<<word
-		end
-		#break 2
-	end
-	
-      end
-	if !found 
-		anagrams<<[word]
-	end
-    end
-return anagrams
+class Class
+  def attr_accessor_with_history(attr_name)
+    attr_name = attr_name.to_s   # make sure it's a string
+    attr_reader attr_name        # create the attribute's getter
+    attr_reader attr_name+"_history" # create bar_history getter
+    class_eval "your code here, use %Q for multiline strings"
+  end
 end
-
-def areAnagram(word1,word2)
- return  word1.downcase.chars.sort==word2.downcase.chars.sort
-end
-
-#a=combine_anagrams( ['cars', 'for', 'potatoes', 'racs', 'four', 'scar', 'creams', 'scream'] )
