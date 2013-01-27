932e08f0780f1f884738363db11e6a7cc4c65412
jmancis <jmancis@gmail.com>
2013-01-27 22:33:48 +0100

    Update hw1_part7.rb

--- hw1_part7.rb
+++ hw1_part7.rb
@@ -1,4 +1,25 @@
 class CartesianProduct
   include Enumerable
-  # YOUR CODE HERE
+  def initialize(enu1,enu2)
+  @enu1=enu1
+	@enu2=enu2
+  end
+  def each
+     @enu1.each do |elt1|
+	@enu2.each do |elt2|
+       	  yield [elt1,elt2]
+	end
+     end
+  end
 end
+
+c = CartesianProduct.new([:a,:b], [4,5])
+c.each { |elt| puts elt.inspect }
+# [:a, 4]
+# [:a, 5]
+# [:b, 4]
+# [:b, 5]
+
+c = CartesianProduct.new([:a,:b], [])
+c.each { |elt| puts elt.inspect }
+# Nothing printed since Cartesian product of anything with an empty collection is empty
